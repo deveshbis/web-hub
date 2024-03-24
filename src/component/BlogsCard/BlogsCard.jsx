e8d8d68db5922a3e6d79c9e77ef4129f967282f2
deveshbis <deveshbiswas.cse@gmail.com>
2024-03-24 17:36:07 +0600

    remove the blog in bookmarks

--- src/component/BlogsCard/BlogsCard.jsx
+++ src/component/BlogsCard/BlogsCard.jsx
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
-
+import { MdDeleteForever } from "react-icons/md";
 import placeHoldeImage from '../../assets/404.jpg'
 
+
 // eslint-disable-next-line react/prop-types
-const BlogsCard = ({blog}) => {
-    const {id, cover_image, title, published_at, description} = blog
+const BlogsCard = ({ blog, deletable,handleDelete }) => {
+    const { id, cover_image, title, published_at, description } = blog;
+    
     return (
-        <Link to = {`/blog/${id}`}  className="max-w-sm mx-auto group hover:no-underline focus:no-underline transition border-2 hover:scale-105 border-primary hover:border-secondary border-opacity-30 ">
-            <img role="presentation" className="object-cover w-full rounded h-44 bg-gray-500" src={cover_image || placeHoldeImage} />
-            <div className="p-6 space-y-2">
-                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
-                <span className="text-xs text-gray-400">{new Date(published_at).toLocaleString()}</span>
-                <p>{description}</p>
-            </div>
-        </Link>
+        <div className="flex relative">
+            <Link to={`/blog/${id}`} className="max-w-sm mx-auto group hover:no-underline focus:no-underline transition border-2 hover:scale-105 border-primary hover:border-secondary border-opacity-30 ">
+                <img role="presentation" className="object-cover w-full rounded h-44 bg-gray-500" src={cover_image || placeHoldeImage} />
+                <div className="p-6 space-y-2">
+                    <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+                    <span className="text-xs text-gray-400">{new Date(published_at).toLocaleString()}</span>
+                    <p>{description}</p>
+                </div>
+            </Link>
+            {deletable && (
+            <div onClick={()=> handleDelete(id)} className="absolute bg-primary p-3 rounded-full hover:scale-105 -top-5 -right-5">
+                <MdDeleteForever 
+                size={20} 
+                className="text-white group-hover:text-primary"></MdDeleteForever></div>
+            )}
+        </div>
     );
 };
-BlogsCard.propTypes ={
+BlogsCard.propTypes = {
     blog: PropTypes.obj
 }
 
--- src/component/Bookmarks/Bookmarks.jsx
+++ src/component/Bookmarks/Bookmarks.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getBlogs } from "../../Utilities";
+import { deleteBlog, getBlogs } from "../../Utilities";
 import BlogsCard from "../BlogsCard/BlogsCard";
 
 
@@ -9,11 +9,17 @@ const Bookmarks = () => {
     useEffect(()=>{
         const storedBlogs = getBlogs()
         setBlogs(storedBlogs)
-    }, [])
+    }, []);
+
+    const handleDelete = id =>{
+        deleteBlog(id);
+        const storedBlogs = getBlogs()
+        setBlogs(storedBlogs)
+    }
     return (
         <div className="grid px-4 sm:px-8 lg:px-12 py-8 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
             {
-                blogs.map(blog => <BlogsCard key={blog.id} blog={blog}></BlogsCard>)
+                blogs.map(blog =>  <BlogsCard handleDelete={handleDelete} deletable ={true} key={blog.id} blog={blog}></BlogsCard>)
             }
 
         </div>
