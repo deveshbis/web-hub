ebf0dcabab25cc69fb4950c1fc4828ec0d12819b
deveshbis <deveshbiswas.cse@gmail.com>
2024-03-24 15:07:37 +0600

    added content page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,34 +4,48 @@ import Home from "../component/Pages/Home";
 import Blogs from "../component/Blogs/Blogs";
 import Blog from "../component/Pages/Blog/Blog";
 import Bookmarks from "../component/Bookmarks/Bookmarks";
+import Content from "../component/Content/Content";
+import Author from "../component/Content/Author";
 
 export const router = createBrowserRouter([
     {
-      path: '/',
-      element: <App></App>,
-      children: [
-        {
-          path: '/',
-          element:<Home></Home>
-        },
-        {
-          path: '/blogs',
-          element: <Blogs></Blogs>,
-          loader: ()=> fetch('https://dev.to/api/articles'),
-        },
-  
-        {
-          path: '/blog/:id',
-          element: <Blog></Blog>,
-          loader: ({params})=> fetch(`https://dev.to/api/articles/${params.id}`),
-        },
-      
-        {
-          path: '/bookmarks',
-          element: <Bookmarks></Bookmarks>,
-        },
-      ],
+        path: '/',
+        element: <App></App>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/blogs',
+                element: <Blogs></Blogs>,
+                loader: () => fetch('https://dev.to/api/articles'),
+            },
+
+            {
+                path: '/blog/:id',
+                element: <Blog></Blog>,
+                loader: ({ params }) => fetch(`https://dev.to/api/articles/${params.id}`),
+                children: [
+                    {
+                        index: true,
+                        element: <Content></Content>,
+                        loader: ({ params }) => fetch(`https://dev.to/api/articles/${params.id}`),
+                    },
+                    {
+                        path: 'author',
+                        element: <Author></Author>,
+                        loader: ({ params }) => fetch(`https://dev.to/api/articles/${params.id}`),
+                    },
+                ],
+            },
+
+            {
+                path: '/bookmarks',
+                element: <Bookmarks></Bookmarks>,
+            },
+        ],
     },
-  
-    
-  ])
+
+
+])
--- src/component/Pages/Blog/Blog.jsx
+++ src/component/Pages/Blog/Blog.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, Outlet, useLoaderData } from "react-router-dom";
 
 
 const Blog = () => {
 
-    const [tabIndex, setTabIndex] =useState(0);
+    const [tabIndex, setTabIndex] = useState(0);
 
     const blog = useLoaderData()
 
@@ -22,49 +22,29 @@ const Blog = () => {
                             <p className="flex-shrink-0 mt-3 text-sm md:mt-0">{comments_count} comments • {public_reactions_count} views</p>
                         </div>
                         <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800">
-                            <Link 
-                            // to='' 
-                            onClick={()=> setTabIndex(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
+                            <Link
+                                to=''
+                                onClick={() => setTabIndex(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                     <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                                 </svg>
                                 <span>Content</span>
                             </Link>
-                            <Link 
-                            // to={`author`} 
-                            onClick={()=> setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
+                            <Link
+                                to={`author`}
+                                onClick={() => setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                     <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                                     <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                                 </svg>
                                 <span>Author</span>
                             </Link>
-                            
+
                         </div>
                     </div>
-                    
+                    <Outlet></Outlet>
                 </article>
-                <div>
-                    <div className="flex flex-wrap py-6 gap-2 border-t border-dashed dark:border-gray-600">
-                        <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#MambaUI</a>
-                        <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#TailwindCSS</a>
-                        <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#Angular</a>
-                    </div>
-                    <div className="space-y-2">
-                        <h4 className="text-lg font-semibold">Related posts</h4>
-                        <ul className="ml-4 space-y-1 list-disc">
-                            <li>
-                                <a rel="noopener noreferrer" href="#" className="hover:underline">Nunc id magna mollis</a>
-                            </li>
-                            <li>
-                                <a rel="noopener noreferrer" href="#" className="hover:underline">Duis molestie, neque eget pretium lobortis</a>
-                            </li>
-                            <li>
-                                <a rel="noopener noreferrer" href="#" className="hover:underline">Mauris nec urna volutpat, aliquam lectus sit amet</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                
             </div>
         </div>
     );
